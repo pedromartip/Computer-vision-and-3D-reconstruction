30d3d187fa0e1486ab69f43b005fbdfd7fc86c1c
Pedro Marti <47566794+pedromartip@users.noreply.github.com>
2023-12-08 02:18:32 +0100

    06 i 07

---
+++ 07a-MatchDescriptors/.ipynb_checkpoints/surf-checkpoint.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img1 = cv.imread('input/box.png',cv.IMREAD_GRAYSCALE)          # queryImage
+img2 = cv.imread('input/box_in_scene.png',cv.IMREAD_GRAYSCALE) # trainImage
+# Initiate SURF detector
+sift = cv.xfeatures2d.SURF_create(400)
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+# BFMatcher with default params
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1,des2,k=2)
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+# cv.drawMatchesKnn expects list of lists as matches.
+img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.imshow(img3),plt.show()
